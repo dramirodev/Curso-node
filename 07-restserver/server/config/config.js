e8151432b0ab41038b5561afaad0467c8f8aa192
David Ramiro Barragan <dramirobarragan@gmail.com>
2020-05-09 12:00:07 +0200

    modificada la duración del token

--- 07-restserver/server/config/config.js
+++ 07-restserver/server/config/config.js
@@ -1,5 +1,5 @@
 process.env.PORT = process.env.PORT || 3000
 
-process.env.CADUCIDAD_TOKEN = 60 * 60 * 24 * 30;
+process.env.CADUCIDAD_TOKEN = '48h';
 process.env.SEED = process.env.SEED || 'este-es-el-seed-desarrollo';
 process.env.GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID || "526341605846-ks2dhpblulp828dkc0l6br5v50fhqi3k.apps.googleusercontent.com"
